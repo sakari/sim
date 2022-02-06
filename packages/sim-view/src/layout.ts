386cfc04fa03b02ac62bf4bf8dcd0fea61389971
sakari <sakariij@gmail.com>
2022-02-06 15:26:07 +0200

    wip. link

--- packages/sim-view/src/layout.ts
+++ packages/sim-view/src/layout.ts
@@ -1,22 +1,47 @@
 import * as cola from 'webcola'
 
 type NodeId = string
+type EdgeId = string
+
 export class Layout {
   private layout: cola.Layout
   private nodes: Array<cola.InputNode>
-  private links: Array<cola.Link<any>>
+  private links: Array<cola.Link<cola.Node | number>>
   private nodeMap: Map<NodeId, any>
+  private edgeMap: Map<EdgeId, any>
 
   constructor() {
     this.layout = new cola.Layout().nodes([]).links([]).avoidOverlaps(true)
     this.links = this.layout.links()
     this.nodes = this.layout.nodes()
     this.nodeMap = new Map()
+    this.edgeMap = new Map()
   }
 
   getNodePosition(id: NodeId): { x: number; y: number } {
     const mapped = this.nodeMap.get(id)
     return mapped
+  }
+
+  addLink(id: EdgeId, from: NodeId, to: NodeId): this {
+    const source = this.nodeMap.get(from)
+    const target = this.nodeMap.get(to)
+    const link = { source, target }
+    this.edgeMap.set(id, link)
+    this.links.push(link)
+    return this
+  }
+
+  removeLink(id: EdgeId): this {
+    const link = this.edgeMap.get(id)
+    if (link) {
+      this.edgeMap.delete(id)
+      const ix = this.links.findIndex(l => l === link)
+      if (ix) {
+        this.links.splice(ix, 1)
+      }
+    }
+    return this
   }
 
   addNode(node: {
